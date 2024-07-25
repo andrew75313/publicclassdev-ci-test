87de7be1a8bffef2284a6833e9ac952dd3f0926a
marlboro09 <taebong8989@gmail.com>
2024-07-25 20:59:45 +0900

    🎉 add : batch 기본설정

--- src/main/java/com/sparta/publicclassdev/PublicClassDevApplication.java
+++ src/main/java/com/sparta/publicclassdev/PublicClassDevApplication.java
@@ -1,5 +1,6 @@
 package com.sparta.publicclassdev;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -10,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableCaching
+@EnableScheduling
+@EnableBatchProcessing
 public class PublicClassDevApplication {
 
     public static void main(String[] args) {
