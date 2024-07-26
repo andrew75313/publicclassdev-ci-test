affb0e4a35271ee807007c85b24f8cfaa951a066
이세비 <lsy205105@naver.com>
2024-07-26 16:08:40 +0900

    🩹 fix : 충돌 수정

--- src/main/java/com/sparta/publicclassdev/domain/community/service/CommunitiesService.java
+++ src/main/java/com/sparta/publicclassdev/domain/community/service/CommunitiesService.java
@@ -23,6 +23,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 @Service
