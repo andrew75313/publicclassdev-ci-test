0a343fb412689c1c1ed539958f47c42a38c92673
marlboro09 <taebong8989@gmail.com>
2024-07-26 21:33:17 +0900

    ✨ update : codeRuns Dto 작성

--- src/main/java/com/sparta/publicclassdev/domain/coderuns/dto/CodeRunsDto.java
+++ src/main/java/com/sparta/publicclassdev/domain/coderuns/dto/CodeRunsDto.java
@@ -1,5 +1,23 @@
 package com.sparta.publicclassdev.domain.coderuns.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class CodeRunsDto {
 
+    private Long codeKatasId;
+    private Long teamsId;
+    private Long usersId;
+    private Long responseTime;
+    private String result;
+
+    public CodeRunsDto(Long codeKatasId, Long teamsId, Long usersId, Long responseTime, String result) {
+        this.codeKatasId = codeKatasId;
+        this.teamsId = teamsId;
+        this.usersId = usersId;
+        this.responseTime = responseTime;
+        this.result = result;
+    }
 }
